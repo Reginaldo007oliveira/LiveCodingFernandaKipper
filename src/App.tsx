cf31ac1d87513858d9b417f957a75968959b23c2
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-05-27 05:32:52 -0300

    Imagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.scss'
+import Logo from './assets/images/logo.svg'
 
 function App() {
   return (
     <main>
-      <h1>my app</h1>
+      <header className='header-container'>
+         <div className='logo-container'>    
+           <img src={Logo} alt="bora" />
+            <div className='logo-text'>
+              <span className='logo-text-bold'>conteudo</span>
+              <span className='logo-text-regular'>conteudo de baixo</span>
+              </div>
+            </div>
+      </header>
     </main>
   )
 }
