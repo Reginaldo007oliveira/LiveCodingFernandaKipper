68b960f26a3299e1375305f72fc8626e66352d91
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-05-27 07:30:51 -0300

    Phosphor

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ArrowUpRight } from 'phosphor-react'
 import './App.scss'
 import { Logo } from './components/Logo'
 
@@ -6,7 +7,16 @@ function App() {
     <main>
       <header className='header-container'>
          <Logo />
-          
+         <nav className='menu-options'>
+          <a href="">Texto 1</a>
+          <a href="">Texto 2</a>
+          <a href="">Texto 3</a>
+         </nav>
+
+         <button>
+          Contact us
+          <ArrowUpRight size={25} weight="bold" />
+          </button>
       </header>
     </main>
   )
