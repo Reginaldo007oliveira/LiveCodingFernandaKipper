22ec23b060ee56c1ac7882a27825cb2d794b6758
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-05-27 17:04:57 -0300

    Botão hover

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,16 @@ function App() {
           <a href="">Texto 3</a>
          </nav>
 
-         <button>
-          Contact us
-          <ArrowUpRight size={25} weight="bold" />
-          </button>
+       <button>
+         <div className='btn-face-white'>
+            Contact us
+            <ArrowUpRight size={25} weight="bold" />
+          </div>
+             <div className='btn-face-purple'>
+                Contact us
+                <ArrowUpRight size={25} weight="bold" />
+             </div>       
+        </button>
       </header>
     </main>
   )
