5abfeb02ba586445805c2dd8ee23888cfa53ce22
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-05-27 05:46:05 -0300

    transformando  svg em component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import './App.scss'
-import Logo from './assets/images/logo.svg'
+import { Logo } from './components/Logo'
 
 function App() {
   return (
     <main>
       <header className='header-container'>
-         <div className='logo-container'>    
-           <img src={Logo} alt="bora" />
-            <div className='logo-text'>
-              <span className='logo-text-bold'>conteudo</span>
-              <span className='logo-text-regular'>conteudo de baixo</span>
-              </div>
-            </div>
+         <Logo />
+          
       </header>
     </main>
   )
